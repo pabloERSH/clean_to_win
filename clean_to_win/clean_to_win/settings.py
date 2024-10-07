4dd60c8433d6b3cd811b6f1543c4a4a14e06f348
Pavel Ershov <pave1ershov@yandex.ru>
2024-10-07 16:50:17 +0300

    change db to psql

--- clean_to_win/clean_to_win/settings.py
+++ clean_to_win/clean_to_win/settings.py
@@ -86,11 +86,14 @@ WSGI_APPLICATION = 'clean_to_win.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'USER': 'django_admin',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
