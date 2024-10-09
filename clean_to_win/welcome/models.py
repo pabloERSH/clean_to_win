f72e68dd77dcdacc97115fdb0cec061d435239be
ilyshks <ilyagorunov.04@gmail.com>
2024-10-09 18:39:55 +0300

    created model for gallery and some fixes

--- clean_to_win/welcome/models.py
+++ clean_to_win/welcome/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+images_path = "static/images/"
+
+
+class Gallery(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=images_path)
+    description = models.TextField(blank=True)
+    upload_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
