d29b8098562c353ae4d649bee62dc6d0201ebd50
Pavel Ershov <pave1ershov@yandex.ru>
2024-10-10 14:46:26 +0300

    added a download of feedbacks to index.html and change it in feedback.html

--- clean_to_win/welcome/views.py
+++ clean_to_win/welcome/views.py
@@ -9,7 +9,9 @@ menu = {
     'Главная': '../'
 }
 
-class Index(TemplateView):
+class Index(ListView):
+    queryset = Feedback.objects.order_by('?')[:2]
+    context_object_name = 'feedbacks'
     template_name = 'welcome/index.html'
     extra_context = {'menu': menu}
 
@@ -18,8 +20,10 @@ class About(TemplateView):
     extra_context = {'menu': menu}
 
 class FeedBack(ListView):
-    model = Feedback
+    #model = Feedback
+    queryset = Feedback.objects.order_by('?')[:7]
     template_name = 'welcome/feedback.html'
+    context_object_name = 'feedbacks'
     extra_context = {'menu': menu}
 
 class Gallery(TemplateView):
