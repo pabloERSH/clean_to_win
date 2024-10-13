b6cb11a88da592e9a14d67e89e483c2e2bc3e7fb
ilyshks <ilyagorunov.04@gmail.com>
2024-10-13 14:44:16 +0300

    added 12 photos for gallery, done upload from DB

--- clean_to_win/welcome/views.py
+++ clean_to_win/welcome/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import GalleryImages
 
 
 menu = {
@@ -8,18 +9,33 @@ menu = {
     'Главная': '../'
 }
 
+
 class Index(TemplateView):
     template_name = 'welcome/index.html'
     extra_context = {'menu': menu}
+
 
 class About(TemplateView):
     template_name = 'welcome/about.html'
     extra_context = {'menu': menu}
 
+
 class FeedBack(TemplateView):
     template_name = 'welcome/feedback.html'
     extra_context = {'menu': menu}
 
-class Gallery(TemplateView):
+
+class Gallery(ListView):
+    model = GalleryImages
     template_name = 'welcome/gallery.html'
+    context_object_name = 'images'
     extra_context = {'menu': menu}
+
+    def get_queryset(self):
+        # Получаем 15 случайных объектов
+        return GalleryImages.objects.order_by("?")[:15]
+
+
+
+
+
