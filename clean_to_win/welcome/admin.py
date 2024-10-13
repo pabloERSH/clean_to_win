6c7daa5902246aef38fd50da5c21b20adee53160
Pavel Ershov <pave1ershov@yandex.ru>
2024-10-13 17:43:25 +0300

    changes in index.html

--- clean_to_win/welcome/admin.py
+++ clean_to_win/welcome/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Feedback
+from .models import Feedback, GalleryImages
 
 
 admin.site.register(Feedback)
+admin.site.register(GalleryImages)
--- clean_to_win/welcome/views.py
+++ clean_to_win/welcome/views.py
@@ -11,12 +11,16 @@ menu = {
     'Главная': '../'
 }
 
-
 class Index(ListView):
-    queryset = Feedback.objects.order_by('?')[:2]
-    context_object_name = 'feedbacks'
+    queryset = GalleryImages.objects.order_by('?')[:3]
     template_name = 'welcome/index.html'
-    extra_context = {'menu': menu}
+    context_object_name = 'images'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['feedbacks'] = Feedback.objects.order_by('?')[:2]
+        return context
 
 class About(TemplateView):
     template_name = 'welcome/about.html'
@@ -24,7 +28,6 @@ class About(TemplateView):
 
 
 class FeedBack(ListView):
-    #model = Feedback
     queryset = Feedback.objects.order_by('?')[:7]
     template_name = 'welcome/feedback.html'
     context_object_name = 'feedbacks'
